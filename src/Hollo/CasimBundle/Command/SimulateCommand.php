588102530cd0d08a1a9b7b16dc014bb4ed2511bc
Michael Holm <hollo@hollo.dk>
2011-08-05 03:24:08 +0200

    Made a simulator with a lot of different options.

--- src/Hollo/CasimBundle/Command/SimulateCommand.php
+++ src/Hollo/CasimBundle/Command/SimulateCommand.php
@@ -14,37 +14,139 @@ class SimulateCommand extends ContainerAwareCommand
   {
     $this
       ->setName('casim:simulate')
-      ->setDescription('Simulate Roulette');
-      #->addArgument('name', InputArgument::OPTIONAL, 'Who do you want to greet?')
-      #->addOption('yell', null, InputOption::VALUE_NONE, 'If set, the task will yell in uppercase letters')
+      ->setDescription('Simulate Roulette')
+      ->addOption('spin', null, InputOption::VALUE_REQUIRED, 'How many spin',10000)
+      ->addOption('cash', null, InputOption::VALUE_REQUIRED, 'How much cash to start with.',2000)
+      ->addOption('start_bet', null, InputOption::VALUE_REQUIRED, 'How big a start bet.',10)
+      ->addOption('max_bet', null, InputOption::VALUE_REQUIRED, 'How much is the biggest gamble.',20000)
+      ;
   }
 
   protected function execute(InputInterface $input, OutputInterface $output)
   {
-    $cash = 2000;
-    $start_bet = 10;
+    $this->start_bet = $input->getOption('start_bet');
+    $max_bet = $input->getOption('max_bet');
+    $spin = $input->getOption('spin');
     $multiplier = 2;
-    $spins = 10;
-    $bet = 'red';
+    $this->cash = $input->getOption('cash');
+    $stats = array(
+      'max_cash' => 0,
+      'max_win' => 0
+    );
+    $win = 0;
+    $this->history = array();
+    $this->game_on = 0;
 
     $wheel = new \Hollo\CasimBundle\Entity\Wheel();
+    $this->curr_bet = false;
+    $this->bet = false;
 
-    for ($i = 0; $i < $spins; $i++) {
-      $curr_bet = $start_bet;
-      $cash -= $bet;
+    for ($i = 0; $i < $spin; $i++) {
 
-      $result = $wheel->getSpin();
+      $output->writeln('');
+      $output->writeln('<comment>Spin number: '.$i.'</comment>');
+      $this->cash -= $this->curr_bet;
 
-      if ($result['color'] == $bet) {
-        $cash += $curr_bet*2;
-        $curr_bet = $start_bet;
+      $this->setBet($output);
+      $result = $this->spin($output, $wheel);
+
+      if ($this->bet == false) {
+        $output->writeln('<comment>Skip turn.</comment>');
       } else {
-        $curr_bet = $curr_bet*$multiplier;
+        if ($result['color'] == $this->bet) {
+          $win = $this->curr_bet*2;
+          $output->writeln('<info>YOU WIN '.$win.'</info>');
+
+          $this->cash += $win;
+          $this->curr_bet = false;
+          $this->game_on = 0;
+        } else {
+          $this->curr_bet = $this->curr_bet*$multiplier;
+          $output->writeln('<comment>Did not win, raising bet to '.$this->curr_bet.' on '.$this->bet.'</comment>');
+        }
+
+        if ($this->curr_bet > $max_bet) {
+          $output->writeln('<error>Bet too high, resetting...</error>');
+          $this->curr_bet = $this->start_bet;
+        }
+
+        if ($this->cash > $stats['max_cash']) $stats['max_cash'] = $this->cash;
+        if ($win > $stats['max_win']) $stats['max_win'] = $win;
+
+        if ($this->cash < 0) {
+          $output->writeln('');
+          $output->writeln('<error>You dont have enough cash :(</error>');
+          break;
+        }
+
+        // this round has finished, only some debugging output after this line
+        $output->writeln('<comment>Bet: '.$this->curr_bet.' on '.$this->bet.'</comment>');
+      }
+
+      $output->writeln('<comment>Cash: '.$this->cash.'</comment>');
+      $output->writeln('');
+    }
+
+    $output->writeln('');
+
+    $output->writeln('<info>Spins: '.$i.'</info>');
+    $output->writeln('<info>Highest win: '.$stats['max_win'].'</info>');
+    $output->writeln('<info>Highest cash: '.$stats['max_cash'].'</info>');
+    $output->writeln('<info>Current cash: '.$this->cash.'</info>');
+  }
+
+  private function setBet($output)
+  {
+    if ($this->game_on) return;
+
+    $trigger = 0;
+
+    if (count($this->history) < 4) {
+      $output->writeln('<comment>Need history, not betting.</comment>');
+      return;
+    }
+
+    foreach ($this->history as $hist) {
+      if (!isset($color)) {
+        $color = $hist['color'];
+      } else {
+        if ($color != $hist['color'])
+          $trigger = 1;
       }
     }
 
-    var_dump($cash);
+    if ($trigger) {
+      $output->writeln('<comment>Bad pattern in last rounds on roulette..</comment>');
+      $this->bet = false;
+      return;
+    }
 
-    $output->writeln('meh');
+    $this->game_on = 1;
+    if ($color == 'red') {
+      $this->bet = 'black';
+    } else {
+      $this->bet = 'red';
+    }
+
+    $this->curr_bet = $this->start_bet;
+    $output->writeln('<comment>Betting on '.$this->curr_bet.' on '.$this->bet.'</comment>');
+  }
+
+  private function addToHistory($result)
+  {
+    if (count($this->history) > 3) {
+      array_shift($this->history);
+    }
+
+    $this->history[] = $result;
+  }
+
+  private function spin($output, $wheel)
+  {
+    $result = $wheel->getSpin();
+    $this->addToHistory($result);
+    $output->writeln('<comment>Roulette: '.$result['color'].'/'.$result['number'].'</comment>');
+
+    return $result;
   }
 }
