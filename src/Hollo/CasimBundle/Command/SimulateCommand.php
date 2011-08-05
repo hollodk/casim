89c1aeae4482fadb43e64078b419c2ac90f130b1
Michael Holm <hollo@hollo.dk>
2011-08-05 13:11:50 +0200

    Made the script more simple.

--- src/Hollo/CasimBundle/Command/SimulateCommand.php
+++ src/Hollo/CasimBundle/Command/SimulateCommand.php
@@ -25,123 +25,97 @@ class SimulateCommand extends ContainerAwareCommand
 
   protected function execute(InputInterface $input, OutputInterface $output)
   {
-    $this->start_bet = $input->getOption('start_bet');
-    $max_bet = $input->getOption('max_bet');
-    $spin = $input->getOption('spin');
-    $this->max_sequence = $input->getOption('max_sequence');
-    $multiplier = 2;
-    $this->cash = $input->getOption('cash');
-    $stats = array(
-      'max_cash' => 0,
-      'min_cash' => $this->cash,
-      'max_win' => 0,
-      'max_bet' => 0
-    );
-    $win = 0;
-    $this->history = array();
-    $this->game_on = 0;
-
+    $this->init($input);
+    $this->dropBet();
     $wheel = new \Hollo\CasimBundle\Entity\Wheel();
-    $this->curr_bet = false;
-    $this->bet = false;
 
-    for ($i = 0; $i < $spin; $i++) {
+    for ($this->spins = 0; $this->spins < $this->spin; $this->spins++) {
 
       $output->writeln('');
-      $output->writeln('<comment>Spin number: '.$i.'</comment>');
+      $output->writeln('<comment>Spin number: '.$this->spins.'</comment>');
 
       $this->setBet($output);
-      $output->writeln('<comment>Cash is '.$this->cash.' we take '.$this->curr_bet.' for next spin, new cash is '.($this->cash-$this->curr_bet).'</comment>');
-      $this->cash -= $this->curr_bet;
       $result = $this->spin($output, $wheel);
 
       if ($this->bet == false) {
         $output->writeln('<comment>Skip turn.</comment>');
       } else {
-        if ($result['color'] == $this->bet) {
-          $win = $this->curr_bet*2;
-          $output->writeln('<info>YOU WIN '.$win.'</info>');
 
-          $this->cash += $win;
-          $this->curr_bet = false;
-          $this->game_on = 0;
+        if ($result['color'] == $this->bet) {
+          $this->winBet($output);
+          $this->dropBet();
+
         } else {
-          $this->curr_bet = $this->curr_bet*$multiplier;
+          $this->raiseBet();
           $output->writeln('<comment>Did not win, raising bet to '.$this->curr_bet.' on '.$this->bet.'</comment>');
         }
 
-        if ($max_bet > 0 && $this->curr_bet > $max_bet) {
-          $output->writeln('<error>Bet too high, resetting...</error>');
-          $this->curr_bet = $this->start_bet;
-        }
-
-        if ($this->cash > $stats['max_cash']) $stats['max_cash'] = $this->cash;
-        if ($this->cash < $stats['min_cash']) $stats['min_cash'] = $this->cash;
-        if ($win > $stats['max_win']) $stats['max_win'] = $win;
-        if ($this->curr_bet > $stats['max_bet']) $stats['max_bet'] = $this->curr_bet;
+        $this->validateBet($output);
 
         if ($this->cash < 0) {
           $output->writeln('');
           $output->writeln('<error>You dont have enough cash :(</error>');
           break;
         }
-
-        if ($this->curr_bet)
-          $output->writeln('<comment>Bet: '.$this->curr_bet.' on '.$this->bet.'</comment>');
       }
-
-      $output->writeln('<comment>Cash: '.$this->cash.'</comment>');
+      $this->writeSpinSummary($output);
     }
 
-    $output->writeln('');
-    $output->writeln('<info>Spins: '.$i.'</info>');
-    $output->writeln('<info>Highest win: '.$stats['max_win'].'</info>');
-    $output->writeln('<info>Highest bet: '.$stats['max_bet'].'</info>');
-    $output->writeln('<info>Highest cash: '.$stats['max_cash'].'</info>');
-    $output->writeln('<info>Lowest cash: '.$stats['min_cash'].'</info>');
-    $output->writeln('<info>Current cash: '.$this->cash.'</info>');
-    $output->writeln('');
-    $output->writeln('<info>Time spent (40 seconds per spin): '.round(($i*40/60/60),2).' hours or '.round(($i*40/60/60/24),2).' days</info>');
-    $profit = $this->cash - $input->getOption('cash');
-    $profit_hour = $profit/($i*40/60/60);
-    $output->writeln('<info>Profit: '.$profit.' ,- or '.round($profit_hour,2).'/h</info>');
+    $this->writeStats($input, $output);
+  }
 
+  private function init($input)
+  {
+    $this->start_bet = $input->getOption('start_bet');
+    $this->max_bet = $input->getOption('max_bet');
+    $this->spin = $input->getOption('spin');
+    $this->max_sequence = $input->getOption('max_sequence');
+    $this->cash = $input->getOption('cash');
+    $this->multiplier = 2;
+    $this->stats = array(
+      'max_cash' => 0,
+      'min_cash' => $this->cash,
+      'max_win' => 0,
+      'max_bet' => 0
+    );
+    $this->win = 0;
+    $this->history = array();
   }
 
   private function setBet($output)
   {
-    if ($this->game_on) return;
+    if (!$this->game_on) {
+      $trigger = 0;
 
-    $trigger = 0;
-
-    if (count($this->history) < $this->max_sequence) {
-      $output->writeln('<comment>Need history, not betting.</comment>');
-      return;
-    }
-
-    foreach ($this->history as $hist) {
-      if (!isset($color)) {
-        $color = $hist['color'];
-      } else {
-        if ($color != $hist['color'])
-          $trigger = 1;
+      if (count($this->history) < $this->max_sequence) {
+        $output->writeln('<comment>Need history, not betting.</comment>');
+        return;
       }
-    }
 
-    if ($trigger) {
-      $output->writeln('<comment>Bad pattern in last rounds on roulette..</comment>');
-      $this->bet = false;
-      return;
-    }
+      foreach ($this->history as $hist) {
+        if (!isset($color)) {
+          $color = $hist['color'];
+        } else {
+          if ($color != $hist['color'])
+            $trigger = 1;
+        }
+      }
 
-    $this->game_on = 1;
-    if ($color == 'red') {
-      $this->bet = 'black';
-    } else {
-      $this->bet = 'red';
-    }
+      if ($trigger) {
+        $output->writeln('<comment>Bad pattern in last rounds on roulette..</comment>');
+        $this->bet = false;
+        return;
+      }
 
-    $this->curr_bet = $this->start_bet;
+      $this->game_on = 1;
+      if ($color == 'red') {
+        $this->bet = 'black';
+      } else {
+        $this->bet = 'red';
+      }
+
+      $this->curr_bet = $this->start_bet;
+    }
     $output->writeln('<comment>Betting '.$this->curr_bet.' on '.$this->bet.'</comment>');
   }
 
@@ -156,10 +130,72 @@ class SimulateCommand extends ContainerAwareCommand
 
   private function spin($output, $wheel)
   {
+    $output->writeln('<comment>Cash is '.$this->cash.' we take '.$this->curr_bet.' for next spin, new cash is '.($this->cash-$this->curr_bet).'</comment>');
+    $this->cash -= $this->curr_bet;
+
+    if ($this->cash < $this->stats['min_cash']) $this->stats['min_cash'] = $this->cash;
+
     $result = $wheel->getSpin();
     $this->addToHistory($result);
     $output->writeln('<comment>Roulette: '.$result['color'].'/'.$result['number'].'</comment>');
 
     return $result;
+  }
+
+  private function dropBet()
+  {
+    $this->curr_bet = false;
+    $this->bet = false;
+    $this->game_on = 0;
+  }
+
+  private function winBet($output)
+  {
+    $this->cash += $this->curr_bet*2;
+    if ($this->curr_bet*2 > $this->stats['max_win']) $this->stats['max_win'] = $this->curr_bet*2;
+    if ($this->cash > $this->stats['max_cash']) $this->stats['max_cash'] = $this->cash;
+
+    $output->writeln('<info>YOU WIN '.($this->curr_bet*2).'</info>');
+  }
+
+  private function setStats()
+  {
+    if ($this->curr_bet > $this->stats['max_bet']) $this->stats['max_bet'] = $this->curr_bet;
+  }
+
+  private function writeStats($input, $output)
+  {
+    $output->writeln('');
+    $output->writeln('<info>Spins: '.$this->spins.'</info>');
+    $output->writeln('<info>Highest win: '.$this->stats['max_win'].'</info>');
+    $output->writeln('<info>Highest bet: '.$this->stats['max_bet'].'</info>');
+    $output->writeln('<info>Highest cash: '.$this->stats['max_cash'].'</info>');
+    $output->writeln('<info>Lowest cash: '.$this->stats['min_cash'].'</info>');
+    $output->writeln('<info>Current cash: '.$this->cash.'</info>');
+    $output->writeln('');
+    $output->writeln('<info>Time spent (40 seconds per spin): '.round(($this->spins*40/60/60),2).' hours or '.round(($this->spins*40/60/60/24),2).' days</info>');
+    $profit = $this->cash - $input->getOption('cash');
+    $profit_hour = $profit/($this->spins*40/60/60);
+    $output->writeln('<info>Profit: '.$profit.' ,- or '.round($profit_hour,2).'/h</info>');
+  }
+
+  private function raiseBet()
+  {
+    $this->curr_bet = $this->curr_bet*$this->multiplier;
+  }
+
+  private function validateBet($output)
+  {
+    if ($this->max_bet > 0 && $this->curr_bet > $this->max_bet) {
+      $output->writeln('<error>Bet too high, resetting...</error>');
+      $this->curr_bet = $this->start_bet;
+    }
+
+    $this->setStats();
+  }
+
+  private function writeSpinSummary($output)
+  {
+    $output->writeln('<comment>Cash: '.$this->cash.'</comment>');
   }
 }
